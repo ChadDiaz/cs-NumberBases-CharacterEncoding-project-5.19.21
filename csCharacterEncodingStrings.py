71fab149cabd0c593c3a142cca4e164b415ba7e2
ChadDiaz <chadcdiaz@gmail.com>
2021-05-19 20:32:43 -0400

    added the first ones I did

---
+++ csCharacterEncodingStrings.py
@@ -0,0 +1,11 @@
+"""
+"In order to store strings in memory, each character in the string must be encoded so that it can be stored as binary.
+ASCII is one example of a character set. Each character in ASCII can be represented by 7 bits (although they are commonly
+stored as 8 bits). Given that, what is the maximum number of characters that could be in the ASCII set?"
+A - 128
+B - 72
+C - 64
+D - 140
+Answer is:
+A - 128
+"""
